1c7629b2898575476da1d4e0b1c66bd01db294d1
Son Roy Almerol <saalmerol@up.edu.ph>
2017-03-14 02:36:43 +0800

    change model backend

--- electionsite/settings.py
+++ electionsite/settings.py
@@ -109,6 +109,8 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTHENTICATION_BACKENDS = ['django.contrib.auth.backends.AllowAllUsersModelBackend']
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.10/topics/i18n/
