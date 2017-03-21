2db58cca2e5805aa0e674a85bf98a32cd55afcfa
Son Roy Almerol <saalmerol@up.edu.ph>
2017-03-21 13:06:21 +0800

    shit

--- electionvote/urls.py
+++ electionvote/urls.py
@@ -3,8 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	url(r'^$', views.redirect, name='redirect'),
-	url(r'^dashboard/$', views.list, name='list'),
+	url(r'^$', views.list, name='list'),
 	url(r'^submit/$', views.submit, name='submit'),
 	url(r'^login/$', views.login, name='login'),
 	url(r'^logout/$', views.logout, name='logout'),
