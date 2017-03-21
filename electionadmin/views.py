431f1a9870d4396406c3ca4565f37517dae5dc9c
Son Roy Almerol <saalmerol@up.edu.ph>
2017-03-22 02:45:53 +0800

    add sms

--- electionadmin/views.py
+++ electionadmin/views.py
@@ -50,9 +50,9 @@ def generate(request):
             #            }
             #response = requests.post('https://post.chikka.com/smsapi/request', data=post_data)
             #content = response.json()
-            if int(content['status']) != 200:
-                messages.error(request, 'SMS Error code: %s, %s' % (content['status'], content['message']))
-                user.delete()
+            #if int(content['status']) != 200:
+            #    messages.error(request, 'SMS Error code: %s, %s' % (content['status'], content['message']))
+            #    user.delete()
         else:
             html_message = loader.render_to_string(
                     'email.html',
